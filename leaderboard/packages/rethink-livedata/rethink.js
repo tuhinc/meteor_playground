9f1cc7866c1534a5b70678711b207fea904551c3
Hack Reactor Students <github@hackreactor.com>
2013-07-29 22:27:38 -0700

    Begins implementing Table constructor

--- leaderboard/packages/rethink-livedata/rethink.js
+++ leaderboard/packages/rethink-livedata/rethink.js
@@ -4,14 +4,11 @@ var RethinkClient = Npm.require('rethinkdb');
 
 Meteor.Rethink = new EventEmitter();
 
-// console.log(url.parse(process.env.MONGO_URL)['host']);
-
 var connection = RethinkClient.connect({
   host:'localhost',
   port: 28015,
   db: 'test'
 }, function(err, conn){
    if(err) throw err;
-   console.log(err);
    Meteor.Rethink.emit('ready');
 });
--- leaderboard/packages/rethink-livedata/tables.js
+++ leaderboard/packages/rethink-livedata/tables.js
@@ -1,24 +1,53 @@
-// var EventEmitter = Npm.require('events').EventEmitter;
-// var r = Npm.require('rethinkdb');
+var EventEmitter = Npm.require('events').EventEmitter;
+var r = Npm.require('rethinkdb');
+var RethinkClient = Npm.require('rethinkdb');
 
-// var CreateTable = function(tableName, options) {
-//   var self = this;
-//   this._table = null;
+var Table = function(tableName, options) {
+  var self = this;
+  this._table = null; 
 
-//   //XXX introduce support for options
-//   //var args = Array.prototype.slice.call(arguments, 1);
+  var makeTable = function(tableName) {
+    RethinkClient.connect({
+      host:'localhost',
+      port: 28015,
+      db: 'test'
+      }, function(err, conn){
+        if(err) throw err;
+        connection = conn;
+        self._table = r.db('test').tableCreate(tableName).run(conn, function(){
+          conn.close();
+        });
+    });
+  };
+};
 
-//   var makeTable = function(tableName) {
-//     self._table = r.db('test').tableCreate(tableName).run();
-//   };
+//todo -- provide support for durability / returnVals arguments
+Table.prototype._insert = function(document, callback) {
+  if (!document._id) {
+    document._id = Random.id();
+  }
 
-//   Meteor.Rethink.on('ready', makeTable);
-//   makeTable(tableName);
-// };
+  // Insert returns an object that contains the following attributes:
 
+  //     inserted - the number of documents that were succesfully inserted;
+  //     replaced - the number of documents that were updated when upsert is used;
+  //     unchanged - the number of documents that would have been modified, except that the new value was the same as the old value when doing an upsert;
+  //     errors - the number of errors encountered while inserting;
+  //     if errors where encountered while inserting, first_error contains the text of the first error;
+  //     generated_keys - a list of generated primary key values;
+  //     deleted and skipped - 0 for an insert operation.
+  RethinkClient.connect({
+    host:'localhost',
+    port: 28015,
+    db: 'test'
+    }, function(err, conn){
+      if(err) throw err;
+      connection = conn;
+      r.db('test').insert(document).run(conn, function() {
+        callback();
+        conn.close();
+      });
+    });
+  };
 
-// Meteor.Rethink.on('ready', function() { console.log('ready'); });
-
-// Meteor.CreateTable = CreateTable;
-
-// Meteor.CreateTable('omg');
+Meteor.Table = Table;
